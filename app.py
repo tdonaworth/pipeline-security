5030a1fee653efa8af657bd7a6f9bb158c862a59
wolfkabal <wolfkabal@gmail.com>
2022-11-09 21:17:31 -0500

    minor text update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World!'
+   return 'Hello World! I\'m just a basic API'
 
 @app.route('/item/new', methods = ['POST'])
 def add_item():
